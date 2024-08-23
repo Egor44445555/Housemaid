0de31513914a52cb3bf87a636bd1075b22f66017
Egor44445555 <55839164+Egor44445555@users.noreply.github.com>
2024-08-23 18:53:37 +0400

    Feature drag and drop in popup

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    private Canvas canvas;
+    private RectTransform rectTransform;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //print("OnBeginDrag");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //print("OnEndDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //print("OnPointerDown");
+    }
+}
