3a2a2132f4451775e83bc604450e0bb601e263a7
Egor44445555 <55839164+Egor44445555@users.noreply.github.com>
2024-08-27 18:27:26 +0400

    Fix Drag and drop popup

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -1,36 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+public class DragDrop : MonoBehaviour, IDragHandler
 {
     private Canvas canvas;
     private RectTransform rectTransform;
+    [SerializeField] private GameObject window;
+
+    Vector3 offset;
+    RectTransform rt;
 
     private void Start()
     {
+        Vector3 minBounds, maxBounds;
+        Collider parentCollider = window.GetComponent<Collider>();
+        minBounds = parentCollider.bounds.min;
+        maxBounds = parentCollider.bounds.max;        
+
         rectTransform = GetComponent<RectTransform>();
         canvas = GetComponentInParent<Canvas>();
     }
 
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        //print("OnBeginDrag");
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        //print("OnEndDrag");
-    }
-
     public void OnDrag(PointerEventData eventData)
     {
-        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
-    }
+        Vector3 minBounds, maxBounds;
+        Collider parentCollider = window.GetComponent<Collider>();
+        minBounds = parentCollider.bounds.min;
+        maxBounds = parentCollider.bounds.max;
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        //print("OnPointerDown");
+        float xPos = Mathf.Clamp(transform.position.x, minBounds.x, maxBounds.x);
+        float yPos = Mathf.Clamp(transform.position.y, minBounds.y, maxBounds.y);
+
+        if (xPos > minBounds.x && xPos < maxBounds.x && yPos > minBounds.y && yPos < maxBounds.y)
+        {
+            rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        }
+        else
+        {
+            float step = 0.05f;
+
+            if (xPos <= minBounds.x)
+            {
+                rectTransform.position = new Vector3(transform.position.x + step, transform.position.y, 0);
+            } else if (xPos >= maxBounds.x)
+            {
+                rectTransform.position = new Vector3(transform.position.x - step, transform.position.y, 0);
+            }
+            else if (yPos <= minBounds.y)
+            {
+                rectTransform.position = new Vector3(transform.position.x, transform.position.y + step, 0);
+            } else if (yPos >= maxBounds.y)
+            {
+                rectTransform.position = new Vector3(transform.position.x, transform.position.y - step, 0);
+            }
+        }
     }
 }
